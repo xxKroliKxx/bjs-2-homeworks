e8e1a984c8681cf3c909c536acc3ae992b352054
rka <russkikh_k@ekb.sima-land.ru>
2021-07-30 23:33:52 +0500

    homework_2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,50 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min, max, sum, avg;
+  sum = 0;
+  avg = 0;
+  min = Infinity;
+  max = -Infinity;
 
-  // Ваш код
+  arr.forEach(item => {
+    sum += item
+    min = min > item ? item : min;
+    max = max < item ? item : max;
+  });
 
-  return { min:min, max:max, avg:avg };
+  avg = Number((sum / arr.length).toFixed(2));
+
+  return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  arr.forEach(item => sum += item);
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  arrOfArr.forEach(item => {
+    let sum = func(item);
+    max = sum > max ? sum : max;
+  });
+
   return max
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max;
+  min = Infinity;
+  max = -Infinity;
+
+  arr.forEach(item => {
+    min = min > item ? item : min;
+    max = max < item ? item : max;
+  });
+
+  return max - min;
 }
