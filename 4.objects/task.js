eaf7660b7eee0f52d857f6cd3e2b701a34eb9174
rka <russkikh_k@ekb.sima-land.ru>
2021-08-11 22:19:12 +0400

    done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name
+  this.gender = gender
+  this.age = age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = []
+  }
+  this.marks.push(mark)
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = []
+  }
+  for (let mark of marks) {
+    this.marks.push(mark)
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let mark of this.marks) sum += mark;
+  return sum / this.marks.length;
+}
+
+Student.prototype.excludeStudent = function (reason) {
+  delete this.marks
+  delete this.subject
+  this.excluded = reason
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks
+  delete this.subject
+  this.excluded = reason
+}
+
